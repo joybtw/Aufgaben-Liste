ad2cf5fc083fb8f8fed9d29b6d31c120909fe11c
joydev <jar.niedling@gmail.com>
2025-06-12 19:17:18 +0200

    12.06.2025 (2)

---
+++ klassen/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AufgabenListe_Forms.Klassen
+{
+    public class User
+    {
+        public Guid Id;
+        public string Name;
+        public string Benutzername;
+        public string Passwort;
+
+        public User(Guid id, string name, string benutzername, string passwort)
+        {
+            Id = id;
+            Name = name;
+            Benutzername = benutzername;
+            Passwort = passwort;
+        }
+    }
+}
---
+++ klassen/UserManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AufgabenListe_Forms.Klassen
+{
+    public class UserManager
+    {
+        private List<User> _users;
+        public UserManager()
+        {
+            _users = new List<User>();
+            // Temp benutzer
+            _users.Add(new User(Guid.NewGuid(), "test", "test", "test"));
+        }
+
+        public User FindUser(string username, string password)
+        {
+            foreach (User user in _users)
+            {
+                if (user.Benutzername.ToLower() == username.ToLower() && user.Passwort == password)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+        public bool BenutzernameFrei(string username) {
+            foreach (User user in _users)
+            {
+                if (user.Benutzername.ToLower() == username.ToLower())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+    }
+}
